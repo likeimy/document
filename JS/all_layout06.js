c5d99c5e5613f8af05c768912b79ef54b231a866
Lee Yunju <100749582+likeimy@users.noreply.github.com>
2022-03-17 17:54:40 +0900

    Add files via upload

---
+++ JS/all_layout06.js
@@ -0,0 +1,20 @@
+$(function(){
+    //////
+
+    // top banner : lee 22. 03. 17 M
+
+    function topBannerHandler() {
+       // $('topBanner').hide(); => display:none;
+       $('.topBanner').slideUp();
+    }
+    $('.topBanner .container i').on('click', topBannerHandler);
+
+    $('.visualSlide').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+    });
+
+    //////
+})
